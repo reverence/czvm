388ab4c67d72c24d4c07076fed6a828564d2eacb
reverence <reverence_cz@163.com>
2017-12-05 17:28:57 +0800

    Create type.h

---
+++ src/type.h
@@ -0,0 +1,20 @@
+/*
+ * type.h
+ *
+ *
+ *      Author: chengzhang
+ */
+
+#ifndef TYPE_H_
+#define TYPE_H_
+
+typedef unsigned int u4;
+typedef unsigned short u2;
+typedef unsigned char u1;
+
+
+/*****************************function****************************************/
+
+int is_primitive_desc_type(char* desc_name);
+
+#endif /* TYPE_H_ */
