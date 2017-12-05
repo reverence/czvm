cff3d47674ec43793188c0e8acc0093e6ddec2fc
reverence <reverence_cz@163.com>
2017-12-05 17:28:08 +0800

    Create mymath.h

---
+++ src/mymath.h
@@ -0,0 +1,32 @@
+/*
+ * mymath.h
+ *
+ *
+ *      Author: chengzhang
+ */
+
+#ifndef MYMATH_H_
+#define MYMATH_H_
+
+/******************************function******************************************************/
+char* big_divide_long(long x,long y,int n);
+
+char* big_multiply(char* str1,char* str2);
+
+long long2float(long a);
+
+long int2float(int a);
+
+long long2double(long a);
+
+long int2double(int a);
+
+float cal_float(long l);
+
+double cal_double(long l);
+
+long float2double(long a);//a---float
+
+long double2float(long a);//a---double
+
+#endif /* MYMATH_H_ */
