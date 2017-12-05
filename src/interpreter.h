f8931993f5d07a5df318b31acb42b4cc79518dde
reverence <reverence_cz@163.com>
2017-12-05 17:22:46 +0800

    Create interpreter.h

---
+++ src/interpreter.h
@@ -0,0 +1,16 @@
+/*
+ * interpreter.h
+ *
+ *
+ *      Author: chengzhang
+ */
+
+#ifndef INTERPRETER_H_
+#define INTERPRETER_H_
+
+#include "czvm.h"
+
+////////////////////////////////////function////////////////////////////////////////////////
+int interpreter_byte_code(class_method* method);
+
+#endif /* INTERPRETER_H_ */
