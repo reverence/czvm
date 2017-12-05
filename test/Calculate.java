05923701d902c152b56fb97274a0afb4daf32664
reverence <reverence_cz@163.com>
2017-12-05 17:33:14 +0800

    Create Calculate.java

---
+++ test/Calculate.java
@@ -0,0 +1,47 @@
+public class Calculate{
+        public static void main(String[] args){
+                //int
+                int a = 3,b = 5;
+                int c = a+b;
+                System.out.println(c);
+                c = a-b;
+                System.out.println(c);
+                c = a*b;
+                System.out.println(c);
+                c = a/b;
+                System.out.println(c);
+
+                //long
+                long l1 = 3l,l2 = 5l;
+                long l3 = l1+l2;
+                System.out.println(l3);
+                l3 = l1 - l2;
+                System.out.println(l3);
+                l3 = l1 * l2;
+                System.out.println(l3);
+                l3 = l1 / l2;
+                System.out.println(l3);
+
+                //float
+                float f1 = 3.0f,f2=5.0f;
+                float f3 = f1+f2;
+                System.out.println(f3);
+                f3 = f1 - f2;
+                System.out.println(f3);
+                f3 = f1 * f2;
+                System.out.println(f3);
+                f3 = f1 / f2;
+                System.out.println(f3);
+
+                //double
+                double d1 = 3.0,d2=5.0;
+                double d3 = d1 + d2;
+                System.out.println(d3);
+                d3 = d1 - d2;
+                System.out.println(d3);
+                d3 = d1 * d2;
+                System.out.println(d3);
+                d3 = d1 / d2;
+                System.out.println(d3);
+        }
+}
